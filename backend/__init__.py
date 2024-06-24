2a130e3e2e98009d9396fadd115b0bdb221468f6
iHeslop <iheslop182@gmail.com>
2024-06-24 17:44:37 +1000

    Setup db connection and added basic routes

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from backend import db_config 
 from backend import routes
--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,23 @@
+from flask import jsonify
 from backend import app
+from backend.db_config import mysql
 
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello World</p>"
+@app.route('/entries', methods=['GET'])
+def get_all_entries():
+    conn = mysql.connect
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM todo_entries")
+    response = cursor.fetchall()
+    cursor.close()
+    return jsonify(response)
+
+
+@app.route('/entries/<id>', methods=['GET'])
+def get_entry_by_id(id):
+    conn = mysql.connect
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM todo_entries WHERE id=%s", [id])
+    response = cursor.fetchone()
+    cursor.close()
+    return jsonify(response)
